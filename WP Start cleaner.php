5cbb909488136874bb87f52f2cee4f18428fdbae
supernews-croatia <126347596+supernews-croatia@users.noreply.github.com>
2023-02-25 18:15:11 +0100

    Update WP Start cleaner.php

--- WP Start cleaner.php
+++ WP Start cleaner.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Start Cleaner
 Description:Ovaj plugin će očistiti Vašu stranicu od početnih totalno nepotrebnih programa. Napravljen uz pomoć ChatGPT , ovaj program briše programe koje bez razloga zauzimaju Vaš prostor na cloudu!
-Version: 2.4.4.3 . posted 25.2.2023. 
+Version: 2.4.4.3 . //prostor za Kvalificirani elektronički vremenski žig//
 Author: Supernews Hrvatska d.o.o.
 */
 // Onemogući Akismet Anti-Spam plugin
